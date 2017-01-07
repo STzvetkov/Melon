1c3ef541ff9885b239a15f841e65751f6d5f6071
Simeon Tzvetkov <simeon.s.tzvetkov@gmail.com>
2017-01-07 11:36:14 +0200

    Add GameClass

---
+++ Carcassonne/Carcassonne/GameClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Carcassonne
+{
+    public class GameClass : Game
+    {
+        private static GameClass game;
+
+        public static GameClass Game
+        {
+            get
+            {
+                if (game == null)
+                {
+                    game = new GameClass();
+                }
+                return game;
+            }
+        }
+
+        //general
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+        private Song backgroundMusic;
+    }
+}
