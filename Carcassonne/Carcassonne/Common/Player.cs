e21e34e1d5cf2e314099e69bc589d0a8c6dbeae8
VVoev <vladovoev@yahoo.co.uk>
2017-01-08 13:33:32 +0200

    added some fields into player class

--- Carcassonne/Carcassonne/Common/Player.cs
+++ Carcassonne/Carcassonne/Common/Player.cs
@@ -8,5 +8,55 @@ namespace Carcassonne.Common
 {
     class Player
     {
+        //fields
+        private string name;
+        private int colorId;
+        private bool hasAvatar;
+
+        //properties
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (String.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("Please enter some name");
+                }
+                if (value.Length > 30)
+                {
+                    throw new ArgumentOutOfRangeException("Name should be no more than 30 symbols");
+                }
+                this.name = value;
+            }
+        }
+        public int ColorID
+        {
+            get { return this.colorId; }
+            set
+            {
+                if(value<0 || value > 4)
+                {
+                    throw new ArgumentNullException("Player color is not correct ,please choose correctly");
+                }
+                this.colorId = value;
+            }
+        }
+
+        public bool HasAvatar
+        {
+            get { return this.hasAvatar; }
+            set { this.hasAvatar = value; }
+        }
+
+        //Constructor
+
+        public Player(string name,int colorId,bool hasAvatar)
+        {
+            this.Name = name;
+            this.ColorID = colorId;
+            this.HasAvatar = hasAvatar;
+        }
+
     }
 }
