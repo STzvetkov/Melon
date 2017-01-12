13834947bd380eb1e394f2f9657ed63b967b78bb
VVoev <vladovoev@yahoo.co.uk>
2017-01-12 15:15:32 +0200

    Added new Class Creature

--- Carcassonne/Carcassonne/GameObjects/Creature.cs
+++ Carcassonne/Carcassonne/GameObjects/Creature.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Carcassonne.GameObjects
 {
-    class Fairy : Creature
+    public abstract class Creature
     {
     }
 }
--- Carcassonne/Carcassonne/GameObjects/Dragon.cs
+++ Carcassonne/Carcassonne/GameObjects/Dragon.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Carcassonne.GameObjects
 {
-    class Dragon
+    class Dragon : Creature
     {
     }
 }
