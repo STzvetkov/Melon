9e0a9f2b3f16cf32046dc8ddf2823ba3b27c5eb7
StArn <StArn@DESKTOP-3JPBP77>
2017-01-14 20:33:31 +0200

    Created IDrawable interface

--- Carcassonne/Carcassonne/Interfaces/IDrawable.cs
+++ Carcassonne/Carcassonne/Interfaces/IDrawable.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Carcassonne.Interfaces
 {
     interface IDrawable
     {
+        Rectangle Rectangle { get; }
+
+        Texture2D Texture { get; }
+
+        void Draw(SpriteBatch spriteBatch);
     }
 }
