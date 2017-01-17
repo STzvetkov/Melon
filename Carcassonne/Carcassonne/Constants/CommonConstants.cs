9754e46525287d01a0863d57443dbe823f96f46c
StArn <StArn@DESKTOP-3JPBP77>
2017-01-17 09:05:52 +0200

    Implemented Menu/Game States

--- Carcassonne/Carcassonne/Constants/CommonConstants.cs
+++ Carcassonne/Carcassonne/Constants/CommonConstants.cs
@@ -14,5 +14,15 @@ namespace Carcassonne.Constants
         public static readonly int windowHeight = 600;
         public static readonly int tileSize = 90;
         public static readonly Rectangle gameDimensions = new Rectangle(0, 0, windowWidth, windowHeight);
+
+        //about menu offset
+        public static readonly int aboutXOffset = 50; 
+        public static readonly int aboutYOffset = 50;
+        public static readonly int aboutTextYOffset = 40;
+
+        //Messages
+        public static readonly string aboutMessage = $"This project was created for CSharp OOP course 2016-autumn Teamwork.";
+        public static readonly string aboutMessageNote = "This project aims no financial benefits, it is for educational purpose only.";
+        public static readonly string aboutMessageUI = $@"All media is used with educational aim only.{Environment.NewLine}All credit goes to their respective owners.{Environment.NewLine}{Environment.NewLine}Developed by Team ""Melon""";
     }
 }
